251df59bb0b1cd91691ba48eb2288e6693a97641
WimmerD190042 <91127911+WimmerD190042@users.noreply.github.com>
2024-01-04 17:10:50 +0100

    Dashboard- Recent Orders

--- frontend/src/app/dashboard/recent-orders/recent-orders.component.ts
+++ frontend/src/app/dashboard/recent-orders/recent-orders.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataService } from '../../data.service';
+import { Order, OrderDto, OrderService } from '../../swagger';
 
 @Component({
   selector: 'app-recent-orders',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './recent-orders.component.html',
-  styleUrl: './recent-orders.component.scss'
+  styleUrl: './recent-orders.component.scss',
 })
 export class RecentOrdersComponent {
+  public orderService = inject(OrderService);
+  public recentOrders = signal<Order[]>([]);
 
+  ngOnInit(): void {
+    this.orderService.orderOrdersGet().subscribe((x) => {
+      const firstTenOrders = x.slice(0, 10);
+      this.recentOrders.set(firstTenOrders);
+      console.log('Recent Orders loaded');
+      console.log(this.recentOrders());
+    });
+  }
 }
