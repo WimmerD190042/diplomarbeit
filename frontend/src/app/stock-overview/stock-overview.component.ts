87a6ede5709104722119fee35ce7fa6ed61ef728
WimmerD190042 <91127911+WimmerD190042@users.noreply.github.com>
2024-03-12 14:08:45 +0100

    Stock being refreshed

--- frontend/src/app/stock-overview/stock-overview.component.ts
+++ frontend/src/app/stock-overview/stock-overview.component.ts
@@ -118,6 +118,7 @@ export class StockOverviewComponent implements OnInit {
       .apiCategoryAddMeatPiecePartPost(meatPiecePart)
       .subscribe(() => {
         console.log('MeatPiecePart added');
+        this.loadStockForCategories();
       });
   }
   deleteMeatPiecePart(meatPiecePart: MeatPiecePart) {
